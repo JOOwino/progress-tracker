9f6a5052dac464b4e042d8a526097d04fe332f7e
JOOwino <jobowino.dev@gmail.com>
2025-07-19 01:07:23 +0300

    Added ability to add book  - Create file if it doesn't exist on the executable Directory.  - If a New File exist, Give user one option of adding resources(books in this scenario)  - Truncate and Write new Content to the files once there is an update  - Single unit of creating prompts for different options

--- main.go
+++ main.go
@@ -4,18 +4,27 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
+	"time"
 )
 
+var newFile bool
+var prompt PromptFields
+
 type Book struct {
-	ID                int    `json:"id"`
+	ID                string `json:"id"`
 	Title             string `json:"title"`
 	Author            string `json:"author"`
+	Chapter           int    `json:"chapter"`
 	TotalPages        int    `json:"total_pages"`
 	CurrentPage       int    `json:"current_page"`
-	PercentageCovered string `json:"percentage_covered"`
+	PercentageCovered int    `json:"percentage_covered"`
 	Status            string `json:"status"`
 }
 
@@ -24,7 +33,7 @@ type ReadTracker struct {
 	File  string `json:"-"`
 }
 
-func ReadingTracker(fileName string) *ReadTracker {
+func NewReadingTracker(fileName string) *ReadTracker {
 	return &ReadTracker{
 		Books: []Book{},
 		File:  fileName,
@@ -36,16 +45,23 @@ func ReadingTracker(fileName string) *ReadTracker {
 //In our scenario we work with Json File
 
 func (rt *ReadTracker) LoadBooks() error {
-	file, err := os.Open(rt.File)
-	if err != nil {
-		//If error is file does not exist://Create file
-		if os.IsExist(err) {
-			return generateNewFile(rt.File)
-		}
-		fmt.Printf("Another error, not of missing file: %v", err)
-		return err
 
+	//Get Executable path so as to create file or read from same directory
+	execPath, err := os.Executable()
+	if err != nil {
+		fmt.Printf("Error fetching the path: %v\n", err)
 	}
+
+	dir := filepath.Dir(execPath)
+	filePath := dir + "/" + rt.File
+	rt.File = filePath
+
+	_, err = os.Stat(filePath)
+	if err != nil {
+		fmt.Printf("PathError: File not found: %v\n", err)
+		return generateNewFile(filePath)
+	}
+	file, err := os.Open(rt.File)
 	defer file.Close()
 
 	decoder := json.NewDecoder(file)
@@ -69,36 +85,123 @@ func init() {
 }
 
 func main() {
-	options := []string{
-		"Add New Book",
-		"Update Reading Progress",
-		"List All Books",
-		"Print Report",
-		"Exit",
+	ptracker := NewReadingTracker("tracker.json")
+	err := ptracker.LoadBooks()
+	if err != nil {
+		fmt.Printf("Error loading file: %v\n", err)
+		if err == io.EOF {
+			newFile = true
+		} else {
+			//No need to continue at this point. No file to load/read from || save to.
+			os.Exit(9)
+		}
+
+	}
+	if newFile {
+		prompt = NewFilePrompt()
+	} else {
+		prompt = ExistingFilePrompt()
 	}
 
-	choice := prompt(options, "Choose an option")
-	fmt.Println("c: %d\n", choice)
+	choice := multipleOptionsPrompt(prompt)
+
+	switch choice {
+	case 1:
+		ptracker.AddBook()
+	default:
+		fmt.Println("Default Option")
+
+	}
 }
 
-func prompt(options []string, prompt string) int {
+func (rt *ReadTracker) AddBook() {
 
-	for i, opt := range options {
+	title := singleOptionPrompt("Book Title: ")
+	author := singleOptionPrompt("Book Author: ")
+	chapters := promptNumberConversion("Number of Chapters: ")
+	pages := promptNumberConversion("Number of Pages: ")
+
+	book := Book{
+		ID:                generateID(),
+		Title:             title,
+		Author:            author,
+		TotalPages:        pages,
+		CurrentPage:       0,
+		PercentageCovered: 0,
+		Chapter:           chapters,
+	}
+
+	rt.Books = append(rt.Books, book)
+	rt.saveFile()
+
+}
+
+func (rt *ReadTracker) saveFile() {
+	file, err := os.OpenFile(rt.File, os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", " ")
+	if err := encoder.Encode(rt.Books); err != nil {
+		fmt.Printf("Error encoding book data: %v\n", err)
+	}
+
+}
+
+func multipleOptionsPrompt(promptData PromptFields) int {
+
+	fmt.Println(promptData.Headline)
+	for i, opt := range promptData.Options {
 		fmt.Printf("[%d]%s\n", i+1, opt)
 	}
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("Select a choice: ")
+		fmt.Print(promptData.ChoicePrefix)
 		input, _ := reader.ReadString('\n')
 		text := strings.TrimSpace(input)
 		index, err := strconv.Atoi(text)
 		//This assumes the user either entered wrong value(not a digit) || entered value greater than the options []string
-		if err == nil && index > 0 && index <= len(options) {
-			return index - 1 //converts to zero based index ?????
+		if err == nil && index > 0 && index <= len(promptData.Options) {
+			//If a valid value Clear Terminal and Return The value to preselect the next prompt
+			clearTerminal()
+			return index //converts to zero based index ?????
 		}
 		fmt.Println("Invalid choice. Enter a valid value")
 
 	}
 
+}
+
+func promptNumberConversion(prompt string) int {
+	value, err := strconv.Atoi(singleOptionPrompt(prompt))
+	if err != nil {
+		fmt.Println("The value should be a number")
+		return promptNumberConversion(prompt)
+	}
+	return value
+}
+
+func singleOptionPrompt(prompt string) string {
+	fmt.Print(prompt)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func generateID() string {
+	return fmt.Sprintf("%d", time.Now().UnixNano())
+}
+
+func clearTerminal() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
---
+++ prompts.go
@@ -0,0 +1,44 @@
+package main
+
+type PromptFields struct {
+	Options      []string
+	Headline     string
+	ChoicePrefix string
+}
+
+func AddBookPrompt() PromptFields {
+	return PromptFields{
+		Options: []string{
+			"Title",
+			"Chapters",
+			"Pages",
+			"Author",
+		},
+		Headline: "Enter Book Details",
+	}
+}
+
+func ExistingFilePrompt() PromptFields {
+	return PromptFields{
+		Options: []string{
+			"Add New Book",
+			"Update Reading Progress",
+			"List All Books",
+			"Print Report",
+			"Exit",
+		},
+		Headline:     "Choose an option:",
+		ChoicePrefix: "Select a choice: ",
+	}
+}
+
+func NewFilePrompt() PromptFields {
+	return PromptFields{
+		Options: []string{
+			"Add New Book",
+			"Exit",
+		},
+		Headline:     "Choose an option:",
+		ChoicePrefix: "Select a choice: ",
+	}
+}
